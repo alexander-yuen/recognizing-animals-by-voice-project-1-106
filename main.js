ad864d14083f5b5605690c639750e8653898fb69
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-09-29 21:59:14 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 https://teachablemachine.withgoogle.com/models/Le7Q3YvJe/model.json
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Le7Q3YvJe/model.json', modelReady);
+}
